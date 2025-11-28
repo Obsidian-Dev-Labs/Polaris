5942eed319f601206f64a84db6fb0eaa664480da
Graham Kelly <gkgoat6700@gmail.com>
2025-11-27 21:14:15 -0500

    get handlers

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,15 @@
-type ObjTy = "object" | "function";
+type ObjTy = "object" | "function" | "symbol";
 type Core = { local: string; type: ObjTy };
 type ObjectRefPacket =
   | [0, ObjTy, string]
   | [1, string | number | boolean | null | undefined];
-type Packet = [0, boolean, string];
+type Packet = [0, boolean, string] | [1, string, ...ObjectRefPacket];
 const rand = crypto.randomUUID.bind(crypto);
 const _WeakRef = WeakRef;
 const deref: <T extends WeakKey>(ref: WeakRef<T>) => T | undefined =
   WeakRef.prototype.deref.call.bind(WeakRef.prototype.deref) as any;
+const isObject = (a: any): boolean =>
+  typeof a === "object" || typeof a === "function" || typeof a === "symbol";
 export class Reactor {
   readonly #coreMap: WeakMap<WeakKey, Core> = new WeakMap();
   readonly #coreMapGet: (a: WeakKey) => Core | undefined =
@@ -22,15 +24,16 @@ export class Reactor {
     this.#remoteReg.register.bind(this.#remoteReg);
   #socket: (msg: any) => any;
   #objects: { [a: string]: WeakRef<any> } = Object.create(null);
+  #remoteObjects: { [a: string]: WeakRef<any> } = Object.create(null);
   #heldObjects: { [a: string]: any } = Object.create(null);
-  #holdObject(a: string) {
+  readonly #holdObject = (a: string) => {
     this.#heldObjects[a] ??= deref(this.#objects[a]);
-  }
-  #releaseObject(a: string) {
+  };
+  readonly #releaseObject = (a: string) => {
     delete this.#heldObjects[a];
-  }
+  };
   #getObjectRef(a: any): ObjectRefPacket {
-    if (typeof a === "object" || typeof a === "function") {
+    if (isObject(a)) {
       const g = this.#coreMapGet(a);
       if (g) return [0, g.type, g.local];
       const s = rand();
@@ -57,7 +60,18 @@ export class Reactor {
   #releaseRemoteObject(a: string) {
     this.#socket([0, false, a]);
   }
+  #proxyPacket(a: ObjectRefPacket) {
+    switch (a[0]) {
+      case 0:
+        return this.#newProxy(a[2], a[1]);
+      case 1:
+        return a[1];
+    }
+  }
   #newProxy(a: string, type: ObjTy) {
+    const d =
+      a in this.#remoteObjects ? deref(this.#remoteObjects[a]) : undefined;
+    if (d !== undefined) return d;
     this.#holdRemoteObject(a);
     const proxy = new Proxy(
       type === "object"
@@ -66,8 +80,18 @@ export class Reactor {
             const self = this;
             const target = new.target;
           },
-      {}
+      {
+        get: (target, p, receiver) => {
+          const packet: ObjectRefPacket = this.#socket([
+            1,
+            a,
+            ...this.#getObjectRef(p),
+          ]);
+          this.#proxyPacket(packet);
+        },
+      }
     );
+    this.#remoteObjects[a] = new _WeakRef(proxy);
     this.#remoteRegister(proxy, a);
     return proxy;
   }
@@ -75,6 +99,17 @@ export class Reactor {
     this.#socket = socket;
   }
   get handler() {
-    return (msg: Packet) => {};
+    return (msg: Packet) => {
+      switch (msg[0]) {
+        case 0:
+          (msg[1] ? this.#holdObject : this.#releaseObject)(msg[2]);
+          return;
+        case 1:
+          const obj = deref(this.#objects[msg[1]]);
+          return this.#getObjectRef(
+            obj?.[this.#getObjectFromRef((((_0: any, _1: any, ...args: ObjectRefPacket) => args) as any)(...msg))]
+          );
+      }
+    };
   }
 }
