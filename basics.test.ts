255d2bb39baa728a115e0f8bfd100604af8f5120
Graham Kelly <gkgoat6700@gmail.com>
2025-11-30 17:14:12 -0500

    simplify

--- basics.test.ts
+++ basics.test.ts
@@ -7,21 +7,21 @@ function spinUp(): {
   guest_handler: Mock<(packet: Packet) => any>;
   readonly i: number;
 } {
-  let r1,
-    r2,
-    h1: Mock<(packet: Packet) => any>,
-    h2: Mock<(packet: Packet) => any>;
+  let host,
+    guest,
+    host_handler: Mock<(packet: Packet) => any>,
+    guest_handler: Mock<(packet: Packet) => any>;
   let i = 0;
   const rand = () => `rand$${i++}`;
-  r1 = new Reactor((m) => h2(m), { rand });
-  r2 = new Reactor((m) => h1(m), { rand });
-  h1 = vi.fn(r1.handler());
-  h2 = vi.fn(r2.handler());
+  host = new Reactor((m) => guest_handler(m), { rand });
+  guest = new Reactor((m) => host_handler(m), { rand });
+  host_handler = vi.fn(host.handler());
+  guest_handler = vi.fn(guest.handler());
   return Object.freeze({
-    host: r1,
-    guest: r2,
-    host_handler: h1,
-    guest_handler: h2,
+    host,
+    guest,
+    host_handler,
+    guest_handler,
     get i() {
       return i;
     },
